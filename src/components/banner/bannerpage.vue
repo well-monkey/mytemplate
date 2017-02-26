67b56420aac6cd0608968c4b1dec9d40c07bc510
well-monkey <459794155@qq.com>
2017-02-26 21:56:33 +0800

    vuetemplate

---
+++ src/components/banner/bannerpage.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <div class="bannerpage">
+        <swiper loop auto :list="demo06_list" :index="demo06_index" @on-index-change="demo06_onIndexChange">      
+        </swiper>
+       <!--  <p>current index: {{demo06_index}}</p> -->
+    </div>
+  </div>
+</template>
+
+<script>
+import { Swiper, GroupTitle, SwiperItem, XButton, Divider } from 'vux'
+const baseList = [{
+  url: 'javascript:',
+  img: 'https://static.vux.li/demo/1.jpg',
+  title: '送你一朵fua'
+}, {
+  url: 'javascript:',
+  img: 'https://static.vux.li/demo/2.jpg',
+  title: '送你一辆车'
+}, {
+  url: 'javascript:',
+  img: 'https://static.vux.li/demo/3.jpg',
+  title: '送你一次旅行'
+}]
+const urlList = baseList.map((item, index) => ({
+  url: 'http://m.baidu.com',
+  img: item.img,
+  title: ``
+}))
+export default {
+  name:'homepage',
+  components: {
+    Swiper,
+    SwiperItem,
+    GroupTitle,
+    XButton,
+    Divider
+  },
+  data () {
+    return {
+      demo06_list: urlList,
+      demo06_index: 0,    
+      swiperItemIndex: 1
+    }
+  },
+  methods:{
+    tohelloPage:function(){
+      this.$router.push({path:"/hello"})
+    },
+    demo06_onIndexChange (index) {
+      this.demo06_index = index
+    },
+  }
+}
+</script>
+
+<style>
+.vux-demo {
+  text-align: center;
+}
+.logo {
+  width: 100px;
+  height: 100px
+}
+</style>
---
+++ src/page/home/home.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div class="homepage">
+        <banner-page></banner-page>
+        <nav-banner></nav-banner>
+        <goods-list></goods-list>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+import bannerPage from '../../components/banner/bannerpage.vue'
+import navBanner from '../../components/banner/navbanner.vue'
+import goodsList from '../../components/goodslist/goodslist.vue'
+export default {
+  name:'homepage',
+  components: {
+      bannerPage,
+      navBanner,
+      goodsList
+  },
+  data () {
+    return {
+      // note: changing this line won't causes changes
+      // with hot-reload because the reloaded component
+      // preserves its current state and we are modifying
+      // its initial state.
+      msg: 'Hello World!'
+    }
+  },
+  methods:{
+    tohelloPage:function(){
+      this.$router.push({path:"/hello"})
+    }
+  }
+}
+</script>
+
+<style>
+.vux-demo {
+  text-align: center;
+}
+.logo {
+  width: 100px;
+  height: 100px
+}
+</style>
